6bba73dd076a5941d449fc0448cc69d2e2f7b4a6
David Marcus <marcusdavid127@gmail.com>
2024-01-05 13:05:42 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Header</h1>
+<div style="height: 500px;width:500px;background-color:gray"></div>
+<h1 id="section-1">Section 1</h1>
+<div style="height: 4000px;width:500px;background-color:gray"></div>
+<a href="#">Go to Top (using #)</a><br /><br />
+<a href="#top">Go to Top (using #top)</a><br /><br />
+<a href="#section-1">Go to Section 1</a>
